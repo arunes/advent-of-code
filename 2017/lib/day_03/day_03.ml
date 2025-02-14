c1aa806db020f61334ffb1f44df59e4aae09dc09
arunes <ali@arunes.com>
2025-02-14 00:22:16 -0600

    feat: 2017, day 3, part 2

--- 2017/lib/day_03/day_03.ml
+++ 2017/lib/day_03/day_03.ml
@@ -1,3 +1,96 @@
+type direction =
+  | East
+  | North
+  | West
+  | South
+
+type point = {
+  x : int;
+  y : int;
+  value: int;
+}
+
+let part_2 target =
+  let get_value x y matrix = 
+    let n_n = 
+      match List.find_opt (fun p -> p.x = x && p.y = (y+1)) matrix with
+      | Some p ->  p.value
+      | None ->  0
+    in
+
+    let n_ne = 
+      match List.find_opt (fun p -> p.x = (x+1) && p.y = (y+1)) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    let n_e = 
+      match List.find_opt (fun p -> p.x = (x+1) && p.y = y) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    let n_se = 
+      match List.find_opt (fun p -> p.x = (x+1) && p.y = (y-1)) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    let n_s = 
+      match List.find_opt (fun p -> p.x = x && p.y = (y-1)) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+    
+    let n_sw = 
+      match List.find_opt (fun p -> p.x = (x-1) && p.y = (y-1)) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    let n_w = 
+      match List.find_opt (fun p -> p.x = (x-1) && p.y = y) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    let n_nw = 
+      match List.find_opt (fun p -> p.x = (x-1) && p.y = (y+1)) matrix with
+      | Some p ->  p.value
+      | None -> 0
+    in
+
+    n_n + n_ne + n_e + n_se + n_s + n_sw + n_w + n_nw
+  in
+
+  let rec fill x y dir matrix = 
+    let new_point = match dir with
+      | East -> {x=x+1; y=y; value=get_value (x+1) y matrix}
+      | North -> {x=x; y=y+1; value=get_value x (y+1) matrix}
+      | West -> {x=x-1; y=y; value=get_value (x-1) y matrix}
+      | South -> {x=x; y=y-1; value=get_value x (y-1) matrix}
+    in 
+
+    let new_dir = match dir with
+      | East -> 
+          if List.exists (fun p -> p.x=new_point.x && p.y = new_point.y + 1) matrix then East else North
+      | North ->
+          if List.exists (fun p -> p.x=new_point.x - 1 && p.y = new_point.y) matrix then North else West
+      | West ->
+          if List.exists (fun p -> p.x=new_point.x && p.y = new_point.y - 1) matrix then West else South
+      | South -> 
+          if List.exists (fun p -> p.x=new_point.x + 1 && p.y = new_point.y) matrix then South else East
+    in   
+
+    if new_point.value >= target then
+      new_point.value
+    else 
+      fill new_point.x new_point.y new_dir (new_point :: matrix)
+    in
+
+  fill 0 0 East [{ x= 0; y=0; value=1 }]
+
+
 let find_distance target = 
   let rec get_distance distance min count = 
     let max = min + count - 1 in
@@ -35,5 +128,5 @@ let find_distance target =
     | _ -> 0
 
 let input = 312051
-let part1 = string_of_int (find_distance input)
-let part2 = "N/A"
+let part1 = string_of_int @@ find_distance input
+let part2 = string_of_int @@ part_2 input
