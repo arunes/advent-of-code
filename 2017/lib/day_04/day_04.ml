3c4acac0168feffd77c3a8383c08ea799b368772
arunes <ali@arunes.com>
2025-02-15 14:16:03 -0600

    feat: 2017, day 4, part 2

--- 2017/lib/day_04/day_04.ml
+++ 2017/lib/day_04/day_04.ml
@@ -1,4 +1,4 @@
-let input = Utils.read_whole_file "./lib/day_04/input.txt" |> String.split_on_char '\n'
+let input = Utils.read_whole_file "./lib/day_04/input.txt" |> String.split_on_char '\n' 
 
 let rec has_dup = function
     | [] -> false
@@ -8,7 +8,21 @@ let rec unique_count = function
   | [] -> 0
   | hd :: tl -> 
       (if hd |> String.split_on_char ' ' |> has_dup then 0 else 1) + (unique_count tl)
-  
+
+
+let rec valid_count = function
+  | [] -> 0
+  | hd :: tl -> 
+      let sorted_words = hd 
+        |> String.split_on_char ' ' 
+        |> List.map (fun w -> w 
+          |> String.to_seq 
+          |> List.of_seq 
+          |> List.sort Char.compare 
+          |> List.to_seq 
+          |> String.of_seq) in
+      
+      (if sorted_words |> has_dup then 0 else 1) + (valid_count tl)
 
 let part1 = string_of_int @@ unique_count input
-let part2 = string_of_int @@ 2
+let part2 = string_of_int @@ valid_count input
