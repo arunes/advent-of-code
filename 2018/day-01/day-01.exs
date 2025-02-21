4b767ab00e7cdbde40e262f75f9ab0d6931767a3
arunes <ali@arunes.com>
2025-02-21 01:02:16 -0600

    feat: 2018, day 1 part 2

--- 2018/day-01/day-01.exs
+++ 2018/day-01/day-01.exs
@@ -1,3 +1,28 @@
-lines = File.stream!("input.txt") |> Stream.map(&String.trim_trailing/1) |> Enum.to_list()
-total = Enum.reduce(lines, 0, fn ln, acc -> Integer.parse(ln) |> elem(0) |> Kernel.+(acc) end)
-IO.puts(total)
+defmodule Day01 do
+  def get_numbers() do
+    lines = File.stream!("input.txt") |> Stream.map(&String.trim_trailing/1) |> Enum.to_list()
+    Enum.map(lines, fn ln -> Integer.parse(ln) |> elem(0) end)
+  end
+
+  def part1(numbers) do
+    Enum.reduce(numbers, 0, fn n, acc -> n + acc end)
+  end
+
+  def part2([head | tail], acc, history) do
+    total = acc + head
+
+    if Enum.member?(history, total) do
+      total
+    else
+      part2(tail, total, [total | history])
+    end
+  end
+
+  def part2([], acc, history) do
+    part2(get_numbers(), acc, history)
+  end
+end
+
+numbers = Day01.get_numbers()
+IO.puts("Day 1, part 1 = #{Day01.part1(numbers)}")
+IO.puts("Day 1, part 1 = #{Day01.part2(numbers, 0, [])}")
